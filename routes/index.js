b707a4ef1adc23bc57e949e37e71b04e3c474351
Maria26193 <mariaislam08@gmail.com>
2024-01-31 16:34:13 +0530

    Added Session and Cookies Code

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,37 @@ var router = express.Router();
 var userModel = require('./users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  req.session.userName ="Altamash";
+  req.session.val =true;
+  // Set Cookies Data Cookies Data use two param (param , value)
+  res.cookie("userName","Maria");
   res.render('index', { title: 'Maria' });
+  console.log(req.cookies);
+
+});
+router.get('/sess',function(req,res){
+  if(req.session.val === true){
+    res.send("You Are Altaanddd");
+  }
 });
 // For Add User
 router.get('/add', async function(req, res, next) {
   const addUser = await userModel.create({
-    name : "Sania",
+    name : "Razia",
     age:"32",
     fatherName: "Badrul Islam",
     motherName: "Shamima Khatoon",
     blogNote: "This is Comment From Sania"
   })
+
+  console.log(req.session);
   res.send(addUser);
 });
+router.get('/destroy', function( req, res){
+  req.session.destroy(function(err){
+    console.log("Session Destroy");
+  })
+})
 // Fetch All Data
 router.get('/list', async function(req, res, next) {
   const allUser = await userModel.find()
